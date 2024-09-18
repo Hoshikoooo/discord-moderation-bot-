61f6fbea2829e6046098e7d64801feaa3b3e0c2b
Hoshiko :(-!!🧙 <60076818+Hoshikoooo@users.noreply.github.com>
2024-09-18 05:53:39 +0530

    Update commands.py

--- commands.py
+++ commands.py
@@ -1,33 +1,52 @@
 import discord
 from discord.ext import commands
+import asyncio
 
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+
+@bot.command()
 async def kick(ctx, member: discord.Member, *, reason=None):
-    await member.kick(reason=reason)
-    await ctx.send(f'Kicked {member.mention}')
+    if ctx.author.guild_permissions.kick_members:
+        await member.kick(reason=reason)
+        await ctx.send(f'{member} has been kicked.')
+    else:
+        await ctx.send('You do not have permission to kick members.')
 
+@bot.command()
 async def ban(ctx, member: discord.Member, *, reason=None):
-    await member.ban(reason=reason)
-    await ctx.send(f'Banned {member.mention}')
+    if ctx.author.guild_permissions.ban_members:
+        await member.ban(reason=reason)
+        await ctx.send(f'{member} has been banned.')
+    else:
+        await ctx.send('You do not have permission to ban members.')
 
+@bot.command()
 async def mute(ctx, member: discord.Member, duration: int, *, reason=None):
-    role = discord.utils.get(ctx.guild.roles, name='Muted')
-    if not role:
-        role = await ctx.guild.create_role(name='Muted')
-        for channel in ctx.guild.channels:
-            await channel.set_permissions(role, speak=False, send_messages=False)
-    await member.add_roles(role, reason=reason)
-    await ctx.send(f'Muted {member.mention} for {duration} minutes')
-    await asyncio.sleep(duration * 60)
-    await member.remove_roles(role)
-    await ctx.send(f'Unmuted {member.mention}')
+    if ctx.author.guild_permissions.manage_roles:
+        mute_role = discord.utils.get(ctx.guild.roles, name='Muted')
+        if mute_role:
+            await member.add_roles(mute_role)
+            await ctx.send(f'{member} has been muted for {duration} minutes.')
+            await asyncio.sleep(duration * 60)  # Mute duration in seconds
+            await member.remove_roles(mute_role)
+            await ctx.send(f'{member} has been unmuted.')
+        else:
+            await ctx.send('Mute role not found.')
+    else:
+        await ctx.send('You do not have permission to mute members.')
 
-async def lock(ctx):
-    for channel in ctx.guild.channels:
+@bot.command()
+async def lock(ctx, channel: discord.TextChannel):
+    if ctx.author.guild_permissions.manage_channels:
         await channel.set_permissions(ctx.guild.default_role, send_messages=False)
-    await ctx.send('Server has been locked.')
+        await ctx.send(f'{channel} has been locked.')
+    else:
+        await ctx.send('You do not have permission to lock channels.')
 
-def setup(bot):
-    bot.add_command(commands.Command(kick, name='kick'))
-    bot.add_command(commands.Command(ban, name='ban'))
-    bot.add_command(commands.Command(mute, name='mute'))
-    bot.add_command(commands.Command(lock, name='lock'))
+bot.run('YOUR_BOT_TOKEN')
